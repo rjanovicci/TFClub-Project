469c63f75e696d9d68d5356d3a228caf1d51ae0c
rjanovicci <rjanovicci@hotmail.com>
2022-03-25 22:48:04 -0300

    feat: add leaderboard/away service

--- app/backend/src/service/leaderboard.ts
+++ app/backend/src/service/leaderboard.ts
@@ -34,7 +34,18 @@ interface IDataPoints {
 const thisTeam = (arrayMatchs: [], searchedTeam: string) => (arrayMatchs.filter((each: IMatchs) =>
   each.homeClub.clubName === searchedTeam || each.awayClub.clubName === searchedTeam));
 
-const getTotalVictories = (arrayMatchs: [], searchedTeam: string) => {
+const getEfficiency = (data: IDataPoints) => {
+  const { totalVictories, totalDraws, totalLosses } = data;
+  const totalPoints = (totalVictories * 3) + totalDraws;
+  const totalGames = totalVictories + totalDraws + totalLosses;
+  const result = Number(((totalPoints / (totalGames * 3)) * 100).toFixed(2));
+
+  return result;
+};
+
+/* ----- /home URL ----- */
+
+const getTotalVictoriesH = (arrayMatchs: [], searchedTeam: string) => {
   const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
   let result = 0;
   thisTeamResult.forEach((each: IMatchs) => {
@@ -45,7 +56,7 @@ const getTotalVictories = (arrayMatchs: [], searchedTeam: string) => {
   return result;
 };
 
-const getTotalDraws = (arrayMatchs: [], searchedTeam: string) => {
+const getTotalDrawsH = (arrayMatchs: [], searchedTeam: string) => {
   const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
   let result = 0;
   thisTeamResult.forEach((each: IMatchs) => {
@@ -56,7 +67,7 @@ const getTotalDraws = (arrayMatchs: [], searchedTeam: string) => {
   return result;
 };
 
-const getTotalLoss = (arrayMatchs: [], searchedTeam: string) => {
+const getTotalLossH = (arrayMatchs: [], searchedTeam: string) => {
   const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
   let result = 0;
   thisTeamResult.forEach((each: IMatchs) => {
@@ -67,7 +78,7 @@ const getTotalLoss = (arrayMatchs: [], searchedTeam: string) => {
   return result;
 };
 
-const getGoalsFavor = (arrayMatchs: [], searchedTeam: string) => {
+const getGoalsFavorH = (arrayMatchs: [], searchedTeam: string) => {
   const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
   let result = 0;
   thisTeamResult.forEach((each: IMatchs) => {
@@ -78,7 +89,7 @@ const getGoalsFavor = (arrayMatchs: [], searchedTeam: string) => {
   return result;
 };
 
-const getGoalsOwn = (arrayMatchs: [], searchedTeam: string) => {
+const getGoalsOwnH = (arrayMatchs: [], searchedTeam: string) => {
   const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
   let result = 0;
   thisTeamResult.forEach((each: IMatchs) => {
@@ -89,29 +100,20 @@ const getGoalsOwn = (arrayMatchs: [], searchedTeam: string) => {
   return result;
 };
 
-const getEfficiency = (data: IDataPoints) => {
-  const { totalVictories, totalDraws, totalLosses } = data;
-  const totalPoints = (totalVictories * 3) + totalDraws;
-  const totalGames = totalVictories + totalDraws + totalLosses;
-  const result = Number(((totalPoints / (totalGames * 3)) * 100).toFixed(2));
-
-  return result;
-};
-
-const receiveData = (arrayMatchs: [], clubName: string) => {
-  const totalVictories = getTotalVictories(arrayMatchs, clubName);
-  const totalDraws = getTotalDraws(arrayMatchs, clubName);
-  const totalLosses = getTotalLoss(arrayMatchs, clubName);
+const receiveDataH = (arrayMatchs: [], clubName: string) => {
+  const totalVictories = getTotalVictoriesH(arrayMatchs, clubName);
+  const totalDraws = getTotalDrawsH(arrayMatchs, clubName);
+  const totalLosses = getTotalLossH(arrayMatchs, clubName);
 
   return { totalVictories, totalDraws, totalLosses };
 };
 
-const getLeaderboard = async (arrayClubs: IClub[], arrayMatchs: []) => {
+const getLeaderboardHome = async (arrayClubs: IClub[], arrayMatchs: []) => {
   const leaderBoard: ILeaderboard[] = [];
   arrayClubs.forEach((eachObject) => {
-    const data = receiveData(arrayMatchs, eachObject.clubName);
-    const gF = getGoalsFavor(arrayMatchs, eachObject.clubName);
-    const gO = getGoalsOwn(arrayMatchs, eachObject.clubName);
+    const data = receiveDataH(arrayMatchs, eachObject.clubName);
+    const gF = getGoalsFavorH(arrayMatchs, eachObject.clubName);
+    const gO = getGoalsOwnH(arrayMatchs, eachObject.clubName);
     leaderBoard.push({ name: eachObject.clubName,
       totalPoints: (data.totalVictories * 3) + data.totalDraws,
       totalGames: data.totalVictories + data.totalLosses + data.totalDraws,
@@ -127,4 +129,94 @@ const getLeaderboard = async (arrayClubs: IClub[], arrayMatchs: []) => {
   return leaderBoard;
 };
 
-export default getLeaderboard;
+/* ----- /away URL ----- */
+
+const getTotalVictoriesA = (arrayMatchs: [], searchedTeam: string) => {
+  const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
+  let result = 0;
+  thisTeamResult.forEach((each: IMatchs) => {
+    if (each.awayClub.clubName === searchedTeam && each.homeTeamGoals < each.awayTeamGoals) {
+      result += 1;
+    }
+  });
+  return result;
+};
+
+const getTotalDrawsA = (arrayMatchs: [], searchedTeam: string) => {
+  const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
+  let result = 0;
+  thisTeamResult.forEach((each: IMatchs) => {
+    if (each.awayClub.clubName === searchedTeam && each.homeTeamGoals === each.awayTeamGoals) {
+      result += 1;
+    }
+  });
+  return result;
+};
+
+const getTotalLossA = (arrayMatchs: [], searchedTeam: string) => {
+  const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
+  let result = 0;
+  thisTeamResult.forEach((each: IMatchs) => {
+    if (each.awayClub.clubName === searchedTeam && each.homeTeamGoals > each.awayTeamGoals) {
+      result += 1;
+    }
+  });
+  return result;
+};
+
+const getGoalsFavorA = (arrayMatchs: [], searchedTeam: string) => {
+  const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
+  let result = 0;
+  thisTeamResult.forEach((each: IMatchs) => {
+    if (each.awayClub.clubName === searchedTeam) {
+      result += each.awayTeamGoals;
+    }
+  });
+  return result;
+};
+
+const getGoalsOwnA = (arrayMatchs: [], searchedTeam: string) => {
+  const thisTeamResult = thisTeam(arrayMatchs, searchedTeam);
+  let result = 0;
+  thisTeamResult.forEach((each: IMatchs) => {
+    if (each.awayClub.clubName === searchedTeam) {
+      result += each.homeTeamGoals;
+    }
+  });
+  return result;
+};
+
+const receiveDataA = (arrayMatchs: [], clubName: string) => {
+  const totalVictories = getTotalVictoriesA(arrayMatchs, clubName);
+  const totalDraws = getTotalDrawsA(arrayMatchs, clubName);
+  const totalLosses = getTotalLossA(arrayMatchs, clubName);
+
+  return { totalVictories, totalDraws, totalLosses };
+};
+
+const getLeaderboardAway = async (arrayClubs: IClub[], arrayMatchs: []) => {
+  const leaderBoard: ILeaderboard[] = [];
+  arrayClubs.forEach((eachObject) => {
+    const data = receiveDataA(arrayMatchs, eachObject.clubName);
+    const gF = getGoalsFavorA(arrayMatchs, eachObject.clubName);
+    const gO = getGoalsOwnA(arrayMatchs, eachObject.clubName);
+    leaderBoard.push({ name: eachObject.clubName,
+      totalPoints: (data.totalVictories * 3) + data.totalDraws,
+      totalGames: data.totalVictories + data.totalLosses + data.totalDraws,
+      totalVictories: data.totalVictories,
+      totalDraws: data.totalDraws,
+      totalLosses: data.totalLosses,
+      goalsFavor: gF,
+      goalsOwn: gO,
+      goalsBalance: gF - gO,
+      efficiency: getEfficiency(data),
+    });
+  });
+  return leaderBoard;
+};
+
+export {
+  getLeaderboardHome,
+  getLeaderboardAway,
+  ILeaderboard,
+};
