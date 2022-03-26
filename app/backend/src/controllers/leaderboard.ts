eeff1914a331b98a6d0a54dff35a4b8c1e5e8ba2
rjanovicci <rjanovicci@hotmail.com>
2022-03-25 22:43:19 -0300

    feat: add /away route

--- app/backend/src/controllers/leaderboard.ts
+++ app/backend/src/controllers/leaderboard.ts
@@ -1,30 +1,44 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-import getLeaderboard from '../service/leaderboard';
+import { getLeaderboardHome, getLeaderboardAway, ILeaderboard } from '../service/leaderboard';
 import { findAllClubs } from '../service/clubs';
 import { findAllMatchs } from '../service/matchs';
 
 const routerLeaderboard = express.Router();
+
+const order = (a: ILeaderboard, b: ILeaderboard): number => {
+  if (a.totalPoints > b.totalPoints) return -1;
+  if (a.totalPoints < b.totalPoints) return 1;
+  if (a.totalVictories > b.totalVictories) return -1;
+  if (a.totalVictories < b.totalVictories) return 1;
+  if (a.goalsBalance > b.goalsBalance) return -1;
+  if (a.goalsBalance < b.goalsBalance) return 1;
+  if (a.goalsFavor > b.goalsFavor) return -1;
+  if (a.goalsFavor < b.goalsFavor) return 1;
+  if (a.goalsOwn > b.goalsOwn) return -1;
+  if (a.goalsOwn > b.goalsOwn) return -1;
+  return 0;
+};
 
 routerLeaderboard.get(
   '/home',
   async (_req: Request, res: Response) => {
     const arrayClubs = await findAllClubs();
     const arrayMatchs = await findAllMatchs('false') as unknown as [];
-    const result = await getLeaderboard(arrayClubs, arrayMatchs);
-    result.sort((a, b): number => {
-      if (a.totalPoints > b.totalPoints) return -1;
-      if (a.totalPoints < b.totalPoints) return 1;
-      if (a.totalVictories > b.totalVictories) return -1;
-      if (a.totalVictories < b.totalVictories) return 1;
-      if (a.goalsBalance > b.goalsBalance) return -1;
-      if (a.goalsBalance < b.goalsBalance) return 1;
-      if (a.goalsFavor > b.goalsFavor) return -1;
-      if (a.goalsFavor < b.goalsFavor) return 1;
-      if (a.goalsOwn > b.goalsOwn) return -1;
-      if (a.goalsOwn > b.goalsOwn) return -1;
-      return 0;
-    });
+    const result = await getLeaderboardHome(arrayClubs, arrayMatchs);
+    result.sort(order);
+
+    return res.status(200).json(result);
+  },
+);
+
+routerLeaderboard.get(
+  '/away',
+  async (_req: Request, res: Response) => {
+    const arrayClubs = await findAllClubs();
+    const arrayMatchs = await findAllMatchs('false') as unknown as [];
+    const result = await getLeaderboardAway(arrayClubs, arrayMatchs);
+    result.sort(order);
 
     return res.status(200).json(result);
   },
